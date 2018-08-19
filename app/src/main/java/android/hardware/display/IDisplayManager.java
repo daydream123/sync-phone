1e759d35d307a8443811aa425c91c79d06103f85
good luck <daydream123@users.noreply.github.com>
2018-08-19 10:51:41 +0800

    Update IDisplayManager.java

--- app/src/main/java/android/hardware/display/IDisplayManager.java
+++ app/src/main/java/android/hardware/display/IDisplayManager.java
@@ -3,10 +3,6 @@ package android.hardware.display;
 import android.os.IBinder;
 import android.view.DisplayInfo;
 
-/**
- * Created by wanjian on 2017/4/4.
- */
-
 public interface IDisplayManager {
     DisplayInfo getDisplayInfo(int i);
 
